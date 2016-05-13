303c85e734cf8cbf059524085a495abe2608b80a
Ksenia <svetlichnayaksenia@gmail.com>
2016-05-13 15:53:00 +0200

    js-correct

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,5 @@
 $('body').on('click', '[href*="#"]', function (e) {
-    let fixedOffset = 60;
+    let fixedOffset = 55;
     $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixedOffset }, 1000);
     e.preventDefault();
 });
